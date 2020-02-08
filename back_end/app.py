4acd39abbf1ac790f1eae468d63eb39266e5e14a
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-02-08 14:54:37 -0800

    complete recommendation

--- back_end/app.py
+++ back_end/app.py
@@ -1,18 +1,38 @@
 from flask import Flask, jsonify
 import json
-from filter import get_trained_model
+from filter import get_model
 import pandas as pd
+from getter import get_list
+import numpy as np
 
 app = Flask(__name__)
-MODEL = get_trained_model()
+MODEL = get_model()
 MOVIES = pd.read_csv('dataset//movies.csv')
 
 
 @app.route('/<user_id>/<mood>/<company>')
 def send_all(user_id, mood, company):
+    movie_list = list()
+    mood = int(mood)
+    company = int(company)
+    user_id = int(user_id)
+
+    if mood == 0 and company == 0:
+        movie_list = get_list(user_id*100+0, MODEL, MOVIES)
+    elif mood == 0 and company == 1:
+        movie_list = get_list(user_id*100+1, MODEL, MOVIES)
+    if mood == 0 and company == 2:
+        movie_list = get_list(user_id*100+2, MODEL, MOVIES)
+    if mood == 1 and company == 0:
+        movie_list = get_list(user_id*100+3, MODEL, MOVIES)
+    if mood == 1 and company == 1:
+        movie_list = get_list(user_id*100+4, MODEL, MOVIES)
+    if mood == 1 and company == 2:
+        movie_list = get_list(user_id*100+5, MODEL, MOVIES)
+
     """ endpoint: takes HTTP request and return movie recommendation(s) """
-    movie_recommendation = ["movie1", "movie2"]
-    return jsonify(movie_recommendation)
+    lst = [int(i) for i in movie_list]
+    return jsonify(lst)
 
 
 if __name__ == '__main__':
--- back_end/noname.py
+++ back_end/noname.py
@@ -7,7 +7,36 @@ ulist = [13, 276, 303]
 #     if df2.item.size > 100:
 #         print(f'{i} : {df2.item.size}')
 
-df2 = df.query(f'user == 13')
-print(df2)
-# print(f'{df2.item.size}')
+df2 = df.query(f'user == 276')
+l1 = list()
+l2 = list()
+l3 = list()
+l4 = list()
+l5 = list()
+l6 = list()
+
+count = 0
+for i in df2.item:
+    if count % 7 == 0:
+        l6.append(i)
+    elif count % 6 == 0:
+        l5.append(i)
+    elif count % 5 == 0:
+        l4.append(i)
+    elif count % 4 == 0:
+        l3.append(i)
+    elif count % 3 == 0:
+        l2.append(i)
+    elif count % 2 == 0:
+        l1.append(i)
+
+    count -= -1
+
+print(l1)
+print(l2)
+print(l3)
+print(l4)
+print(l5)
+print(l6)
+
 
